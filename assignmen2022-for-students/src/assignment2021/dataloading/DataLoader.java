3c2088ba4b49b859ee313293fedd62b144aa7aed
TomPearson38 <tomapearson38@gmail.com>
2022-04-30 17:14:49 +0100

    Added functionality for reading data from files :)

--- assignmen2022-for-students/src/assignment2021/dataloading/DataLoader.java
+++ assignmen2022-for-students/src/assignment2021/dataloading/DataLoader.java
@@ -6,36 +6,64 @@ import java.util.ArrayList;
 import assignment2021.codeprovided.dataloading.AbstractDataLoader;
 import assignment2021.codeprovided.dataloading.DataParsingException;
 import assignment2021.codeprovided.fitnesstracker.Participant;
+import assignment2021.codeprovided.fitnesstracker.measurements.Measurement;
+import assignment2021.codeprovided.fitnesstracker.measurements.MeasurementType;
+import assignment2021.codeprovided.fitnesstracker.measurements.MeasurementFactory;
+
 
 public class DataLoader extends AbstractDataLoader {
 
 	@Override
 	public Participant loadDataLines(List<String> lines) throws DataParsingException {
-		// TODO implement
 		String firstLine = lines.get(0);
 		String[] firstLineSplit = firstLine.split(",");
 		Participant participant = new Participant(firstLineSplit[0], Integer.parseInt(firstLineSplit[1]), firstLineSplit[2]);
 		
 		//Therefore it removes the need to check for it in the loop.
 		lines.remove(0);
-		String categoryName;
+		MeasurementType mt = MeasurementType.DISTANCE;
+		ArrayList<String> currentTrackers = new ArrayList<String>();
 		
 		for(String line : lines) {
 			String[] currentLine = line.split(CELL_SEPARATOR);
 			if(currentLine.length == 1) {
 				//Category Name is listed.
-				categoryName = currentLine[0];
+				mt = MeasurementType.fromMeasurementName(currentLine[0]);
 			}
-			else if(currentLine[0] == "Count") {
+			else if(currentLine[0].equals("Count")) {
 				//Tracker Names are listed
-				ArrayList<String> names = new ArrayList<String>();
-				for(int i = 1; i < currentLine.length-1; i++) {
-					names.add(currentLine[i]);
+				currentTrackers = new ArrayList<String>();
+				for(int i = 1; i < currentLine.length; i++) {
+					currentTrackers.add(currentLine[i]);
 				}
+				
 			}
-			else if(currentLine[0])
+			else if(isStringInt(currentLine[0]) && (currentLine.length - 1 == (currentTrackers.size()))) {
+			     for(int i = 1; i < currentLine.length; i++) {
+			    	 Measurement currentMeasurement = MeasurementFactory.createMeasurement(mt, Integer.parseInt(currentLine[0]), currentLine[i]); 
+			    	 participant.addMeasurementToTracker(currentTrackers.get(i-1), currentMeasurement);
+			     }
+			}
 		}
-		return null;
+		return participant;
 	}
-
+	
+	private static boolean isStringInt(String input) {
+	    if (input == null) {
+	        return false;
+	    }
+	    int length = input.length();
+	    if (length == 0) {
+	        return false;
+	    }
+	    
+	    for (int i = 0; i < length; i++) {
+	        char digit = input.charAt(i);
+	        if (!('0' < digit && digit < '9')) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
 }

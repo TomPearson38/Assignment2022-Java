e69c3ecbb458a90b908818a8bfa86cf6dac189ab
TomPearson38 <tomapearson38@gmail.com>
2022-05-11 15:30:36 +0100

    Finished Implementing the fitness questions

--- assignmen2022-for-students/src/assignment2021/gui/GUIPanel.java
+++ assignmen2022-for-students/src/assignment2021/gui/GUIPanel.java
@@ -1,5 +1,10 @@
 package assignment2021.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Arrays;
 import java.util.Collection;
 
 import assignment2021.codeprovided.fitnesstracker.Participant;
@@ -16,8 +21,48 @@ public class GUIPanel extends AbstractGUIPanel {
 	}
 
 	@Override
+	protected void loadTrackers() {
+		Collection<String> trackerNames = ((Participant) participants.toArray()[0]).getAllTrackerNames();
+		trackersArray = new String[trackerNames.size() + 1];
+		trackersArray[0] = "all";
+		int i = 1;
+		for (String trackerName : trackerNames) {
+			trackersArray[i] = trackerName;
+			i++;
+		}
+		System.out.println(Arrays.toString(trackersArray));
+	}
+	
+	@Override
+	protected void loadParticipantsNames() {
+		int i = 0;
+		participantsArray = new String[participants.size()];
+		for (Participant p : participants) {
+			participantsArray[i] = p.getName();
+			i++;
+		}
+		System.out.println(Arrays.toString(participantsArray));
+	}
+	
+	
+	@Override
 	public void addListeners() {
-		// TODO add the event handlers here
+		//Add curves
+		addCurvesButton.addActionListener(new pageEventListener());
+		
+		//Remove curves
+		removeAllCurvesButton.addActionListener(new pageEventListener());
+				
+		//Shows the increments in the data
+		cbIncrements.addItemListener(new checkBoxListener());
+		
+		//Updates the data to when these are changed
+		cbAverageValue.addItemListener(new checkBoxListener());
+		
+		cbMaxMinValues.addItemListener(new checkBoxListener());
+		
+		//Change the measurement Type displayed to whatever is listed
+		comboListMeasurementType.addActionListener(new pageEventListener());
 	}
 
 	@Override
@@ -55,5 +100,33 @@ public class GUIPanel extends AbstractGUIPanel {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	
+	class pageEventListener implements ActionListener {
 
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+			if(source == addCurvesButton) {
+				
+			}
+			else if(source == removeAllCurvesButton) {
+				
+			}
+		}
+		
+	}
+	
+	class checkBoxListener implements ItemListener {
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 }
+
+
--- assignmen2022-for-students/src/assignment2021/handoutquestions/FitnessQuestions.java
+++ assignmen2022-for-students/src/assignment2021/handoutquestions/FitnessQuestions.java
@@ -3,11 +3,13 @@ package assignment2021.handoutquestions;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.TreeSet;
 
 import assignment2021.codeprovided.fitnesstracker.Participant;
 import assignment2021.codeprovided.fitnesstracker.Tracker;
@@ -37,17 +39,17 @@ public class FitnessQuestions extends AbstractFitnessQuestions {
 		output += "\nQ11. The total count of energy expenditure measurements for fitness tracker FT1 for the whole dataset:  " + getEEMCountForFT1();
 		output += "\nQ12. The total count of steps measurements for fitness trackers FT2, FT3 and FT4 for the whole dataset:  " + getStepsMCountForFT234();
 		output += "\nQ13. The total count of distance measurements for fitness tracker FT5 for the whole dataset:  " + getDistanceMCountForFT5();
-		output += "\nQ14. The list of participant/s with the highest single measurement of steps (across trackers) and the corresponding number of steps:  ";
-		output += "\nQ15. The list of participant/s with the highest single measurement of walked distance (across trackers) and the corresponding distance:  ";
-		output += "\nQ16. The global average number of steps for FT1 for the whole dataset:  ";
-		output += "\nQ17. The list of participant/s with an average individual participant number of steps (for FT1) above the global average number of steps for FT1 (provide average number of steps value per participant):  ";
-		output += "\nQ18. The list of participant/s with an average individual participant number of steps (for FT1) below the global average number of steps for FT1 (provide average number of steps value per participant):  ";
-		output += "\nQ19. The global average energy expenditure for FT2 and FT3, for the whole dataset:  ";
-		output += "\nQ20. The list of participant/s with an average individual participant energy expenditure (for FT2 and FT3) above the global average energy expenditure for FT2 and FT3 (provide average energy expenditure value per participant):  ";
-		output += "\nQ21. The list of participant/s with an average individual participant energy expenditure (for FT2 and FT3) below the global average energy expenditure for FT2 and FT3 (provide average energy expenditure value per participant):  ";
-		output += "\nQ22. The global average heart rate for the whole dataset:  ";
-		output += "\nQ23. The list of participant/s with an average individual participant heart rate above the global average heart rate (provide average heart rate value per participant):  ";
-		output += "\nQ24. The list of participant/s with an average individual participant heart rate below the global average heart rate (provide average heart rate value per participant):  ";
+		output += "\nQ14. The list of participant/s with the highest single measurement of steps (across trackers) and the corresponding number of steps:  " + getHighestNumberOfStepsParticipants();
+		output += "\nQ15. The list of participant/s with the highest single measurement of walked distance (across trackers) and the corresponding distance:  " + getHighestWalkedDistanceParticipants();
+		output += "\nQ16. The global average number of steps for FT1 for the whole dataset:  " + getGlobalAverageStepsForFT1();
+		output += "\nQ17. The list of participant/s with an average individual participant number of steps (for FT1) above the global average number of steps for FT1 \n	(provide average number of steps value per participant):  " + getAvgStepsAboveGlobalParticipantsForFT1();
+		output += "\nQ18. The list of participant/s with an average individual participant number of steps (for FT1) below the global average number of steps for FT1 \n	(provide average number of steps value per participant):  " + getAvgStepsBelowGlobalParticipantsForFT1();
+		output += "\nQ19. The global average energy expenditure for FT2 and FT3, for the whole dataset:  " + getGlobalAverageEEForFT2FT3();
+		output += "\nQ20. The list of participant/s with an average individual participant energy expenditure (for FT2 and FT3) above the global average energy expenditure \n	for FT2 and FT3 (provide average energy expenditure value per participant):  " + getAvgEEAboveGlobalParticipantsForFT2FT3();
+		output += "\nQ21. The list of participant/s with an average individual participant energy expenditure (for FT2 and FT3) below the global average energy expenditure \n	for FT2 and FT3 (provide average energy expenditure value per participant):  " + getAvgEEBelowGlobalParticipantsForFT2FT3();
+		output += "\nQ22. The global average heart rate for the whole dataset:  " + getGlobalAverageHR();
+		output += "\nQ23. The list of participant/s with an average individual participant heart rate above the global average heart rate \n	(provide average heart rate value per participant):  " + getAvgHRAboveGlobalParticipants();
+		output += "\nQ24. The list of participant/s with an average individual participant heart rate below the global average heart rate \n	(provide average heart rate value per participant):  " + getAvgHRBelowGlobalParticipants();
 		return output;
 	}
 
@@ -231,68 +233,232 @@ public class FitnessQuestions extends AbstractFitnessQuestions {
 
 	@Override
 	public Set<String> getHighestNumberOfStepsParticipants() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<String> contents = getHighest(MeasurementType.STEPS);
+		return contents;
 	}
 
 	@Override
 	public Set<String> getHighestWalkedDistanceParticipants() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<String> contents = getHighest(MeasurementType.DISTANCE);
+		return contents;
 	}
-
+	
+	private Set<String> getHighest(MeasurementType mt){
+		Collection<Participant> participants = super.getParticipants();
+		Set<String> currentHighestParticipants = new TreeSet<String>();
+		int currentHighestNum = 0;
+		
+		for (Participant current : participants) {
+			for(Tracker currentTracker : current.getAllTrackers()) {
+				List<Measurement> desiredMeasurments = currentTracker.getMeasurementsForType(mt);
+				for(Measurement currentMeasurement : desiredMeasurments) {
+					int currentMeasureInt = (currentMeasurement.getValue()).intValue();
+					if(currentMeasureInt > currentHighestNum) {
+						currentHighestNum = currentMeasureInt;
+						currentHighestParticipants = new TreeSet<String>();
+						currentHighestParticipants.add(current.getName() + " With Value of " + currentHighestNum);
+					}
+					else if(currentMeasureInt == currentHighestNum) {
+						if(!currentHighestParticipants.contains(current.getName())) {
+							currentHighestParticipants.add(current.getName() + " With Value of " + currentHighestNum);
+						}
+					}
+				}
+			}
+		}
+	
+		return currentHighestParticipants;
+	}
+	
 	@Override
 	public double getGlobalAverageStepsForFT1() {
-		// TODO Auto-generated method stub
-		return 0;
+		Collection<Participant> participants = super.getParticipants();
+		Collection<String> trackers = new ArrayList<String>();
+		trackers.add("FT1");
+		double average = averageCalculator(participants, trackers, MeasurementType.STEPS, false);
+		return average;
+	}
+	
+	private double averageCalculator(Collection<Participant> participants, Collection<String> desiredTrackers, MeasurementType mt, boolean overallAverage) {
+		double total = 0.0;
+		int count = 0;
+		double max = 0.0;
+		for (Participant current : participants) {
+			try {
+				for(String desiredTracker : desiredTrackers) {
+					Tracker currentTrack = current.getTracker(desiredTracker);
+					List<Measurement> relevantMeasurements = currentTrack.getMeasurementsForType(mt);
+
+					if(!relevantMeasurements.isEmpty()) {
+						if(!overallAverage) {
+							max = 0;
+							for(Measurement currentMeasure : relevantMeasurements) {
+								if(currentMeasure.getValue().doubleValue() > max) {
+									max = currentMeasure.getValue().doubleValue();
+								}
+							}
+							
+							total += max;
+							count++;
+						}
+						else {
+							for(Measurement currentMeasure : relevantMeasurements) {
+								total += currentMeasure.getValue().doubleValue();
+								count++;
+							}
+						}
+						
+						
+					}
+				}
+			}	
+			catch (NoSuchElementException e) {
+				
+			}
+		}
+		
+		double average = total/count;
+		
+		System.out.println("" + total + "/" + count + "=" + average);
+		
+		return average;
 	}
 
 	@Override
 	public List<String> getAvgStepsAboveGlobalParticipantsForFT1() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> ParticipantsAboveAvg = new ArrayList<String>();
+		Collection<String> trackers = new ArrayList<String>();
+		trackers.add("FT1");
+		ParticipantsAboveAvg = findParticipantsAboveOrBelowAverage(trackers, MeasurementType.STEPS, true, false);
+		return ParticipantsAboveAvg;
 	}
 
+	private List<String> findParticipantsAboveOrBelowAverage(Collection<String> trackers, MeasurementType mt, boolean bigger, boolean overallAverage){
+		Collection<Participant> participants = super.getParticipants();
+		double average = averageCalculator(participants, trackers, mt, overallAverage);
+		ArrayList<String> listOfParticipantsAboveOrBelowAvg = new ArrayList<String>();
+		
+		for(Participant currentParticipant : participants) {
+			Collection<Participant> par = new ArrayList<Participant>();
+			par.add(currentParticipant);
+			double participantAverage = averageCalculator(par, trackers, mt, overallAverage);
+			if(biggerOrSmaller(bigger, participantAverage, average)) {
+				listOfParticipantsAboveOrBelowAvg.add(currentParticipant.getName() + ", Value = " + participantAverage);
+			}
+		}
+			
+		return listOfParticipantsAboveOrBelowAvg;
+	}
+	
+	private boolean biggerOrSmaller(boolean bigger, double currentAverage, double overallAverage) {
+		if(bigger == true) {
+			if(currentAverage > overallAverage) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			if(currentAverage < overallAverage) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	
 	@Override
 	public List<String> getAvgStepsBelowGlobalParticipantsForFT1() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> ParticipantsBelowAvg = new ArrayList<String>();
+		Collection<String> trackers = new ArrayList<String>();
+		trackers.add("FT1");
+		ParticipantsBelowAvg = findParticipantsAboveOrBelowAverage(trackers, MeasurementType.STEPS, false, false);
+		return ParticipantsBelowAvg;
 	}
 
 	@Override
 	public double getGlobalAverageEEForFT2FT3() {
-		// TODO Auto-generated method stub
-		return 0;
+		Collection<Participant> participants = super.getParticipants();
+		Collection<String> trackers = new ArrayList<String>();
+		trackers.add("FT2");
+		trackers.add("FT3");
+		double average = averageCalculator(participants, trackers, MeasurementType.ENERGY_EXPENDITURE, false);
+		return average;
 	}
 
 	@Override
 	public List<String> getAvgEEAboveGlobalParticipantsForFT2FT3() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> ParticipantsBelowAvg = new ArrayList<String>();
+		Collection<String> trackers = new ArrayList<String>();
+		trackers.add("FT2");
+		trackers.add("FT3");
+		ParticipantsBelowAvg = findParticipantsAboveOrBelowAverage(trackers, MeasurementType.ENERGY_EXPENDITURE, true, false);
+		return ParticipantsBelowAvg;
 	}
 
 	@Override
 	public List<String> getAvgEEBelowGlobalParticipantsForFT2FT3() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> ParticipantsBelowAvg = new ArrayList<String>();
+		Collection<String> trackers = new ArrayList<String>();
+		trackers.add("FT2");
+		trackers.add("FT3");
+		ParticipantsBelowAvg = findParticipantsAboveOrBelowAverage(trackers, MeasurementType.ENERGY_EXPENDITURE, false, false);
+		return ParticipantsBelowAvg;
 	}
 
 	@Override
 	public double getGlobalAverageHR() {
-		// TODO Auto-generated method stub
-		return 0;
+		Collection<Participant> participants = super.getParticipants();
+		Collection<String> trackers = new ArrayList<String>();
+		
+		for(Participant currentPar : participants) {
+			for(String name : currentPar.getAllTrackerNames()) {
+				if(!trackers.contains(name)) {
+					trackers.add(name);
+				}
+			}
+		}
+				
+		double average = averageCalculator(participants, trackers, MeasurementType.HEART_RATE, true);
+		return average;
 	}
 
 	@Override
 	public List<String> getAvgHRAboveGlobalParticipants() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Participant> participants = super.getParticipants();
+		List<String> ParticipantsAboveAvg = new ArrayList<String>();
+		Collection<String> trackers = new ArrayList<String>();
+		
+		for(Participant currentPar : participants) {
+			for(String name : currentPar.getAllTrackerNames()) {
+				if(!trackers.contains(name)) {
+					trackers.add(name);
+				}
+			}
+		}
+		
+		ParticipantsAboveAvg = findParticipantsAboveOrBelowAverage(trackers, MeasurementType.HEART_RATE, true, true);
+		return ParticipantsAboveAvg;
 	}
 
 	@Override
 	public List<String> getAvgHRBelowGlobalParticipants() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Participant> participants = super.getParticipants();
+		List<String> ParticipantsAboveAvg = new ArrayList<String>();
+		Collection<String> trackers = new ArrayList<String>();
+		
+		for(Participant currentPar : participants) {
+			for(String name : currentPar.getAllTrackerNames()) {
+				if(!trackers.contains(name)) {
+					trackers.add(name);
+				}
+			}
+		}
+		
+		ParticipantsAboveAvg = findParticipantsAboveOrBelowAverage(trackers, MeasurementType.HEART_RATE, false, true);
+		return ParticipantsAboveAvg;
 	}
 
 }
